b1afc9836c938d8ac80cb24d14bf0e065b105569
ahkamulbd <ahkamulbd@gmail.com>
2023-03-08 20:46:27 +0600

    Problem1 solved

--- js/problem1.js
+++ js/problem1.js
@@ -1,17 +1,31 @@
 /*
 Problem 1: Let’s play a mind game
 
-তোমাকে একটা function দেওয়া হবে called mindGame(” যা ইনপুট হিসেবে একটা positive number নিবে।")
+        তোমাকে একটা function দেওয়া হবে called mindGame(” যা ইনপুট হিসেবে একটা positive number নিবে।")
 
-এখন তোমার task : তোমাকে সেই নাম্বার এর সাথে 3 গুন করে, তারপর 10 যোগ করে, তারপর 2 দিয়ে ভাগ করে, তারপর 5 বিয়োগ করে, যা আউটপুট আসবে তা return করতে হবে
+        এখন তোমার task : তোমাকে সেই নাম্বার এর সাথে 3 গুন করে, তারপর 10 যোগ করে, তারপর 2 দিয়ে ভাগ করে, তারপর 5 বিয়োগ করে, যা আউটপুট আসবে তা return করতে হবে
 
-Sample Input & Output
+        Sample Input & Output
 
-Input: 5;  Output: 7.5
+        Input: 5;  Output: 7.5
 
-Input: 50;  Output: 75
+        Input: 50;  Output: 75
 
-Input: 33; Output: 49.5
-
+        Input: 33; Output: 49.5
 */
+
+function mindGame(number) {
+    let numberMultiplied = number * 3;
+    let numberAdded = numberMultiplied + 10;
+    let numberDivided = numberAdded / 2;
+    let numberSubtracted = numberDivided - 5;
+    let numberSubtractedFixed = numberSubtracted.toFixed(1);
+
+    return numberSubtractedFixed;
+}
+
+const result = mindGame(33);
+console.log(result);
+
+
 
